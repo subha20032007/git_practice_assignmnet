fb826ae11077913e8dfd06aceff24392e06b5873
subhakanta panda <meunknown466@gmail.com>
2022-09-14 21:59:21 +0530

    this is my msg

---
+++ palin.js
@@ -0,0 +1,7 @@
+//create a palindrome project for me
+var i =0 ;
+if(i%2==0){
+console.log("yes")
+}else{
+    console.log("no")
+}
---
+++ prime.js
@@ -0,0 +1,8 @@
+//create a prime number problem
+var i =1;
+var num = 13
+if(13%i==2){
+    console.log("yes")
+    }else{
+        console.log("no")
+    }
